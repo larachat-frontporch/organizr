d9f0cca2a131e9cf26c5f5d1295b4d9f9272f30f
Leo Sjöberg <leo.sjoberg@gmail.com>
2016-01-20 01:01:46 +0200

    Created the function to generate a yearly bitmap from a weekly one.
    This does currently not deal with overflow days nor timezones

--- app/Spacetime/YearAvailabilityBitmap.php
+++ app/Spacetime/YearAvailabilityBitmap.php
@@ -3,16 +3,22 @@
 namespace App\Spacetime;
 
 
+use Carbon\Carbon;
 use GMP;
 
 class YearAvailabilityBitmap
 {
+    /**
+     * The underlying GMP object containing the binary to operate on.
+     *
+     * @var \GMP
+     */
     protected $binary;
 
     /**
      * Initialise the bitmap, ensure we always have a GMP object.
      *
-     * @param $binary
+     * @param  \GMP|string  $binary
      */
     public function __construct($binary)
     {
@@ -23,11 +29,39 @@ class YearAvailabilityBitmap
         $this->binary = $binary;
     }
 
+    /**
+     * Create a bitmap for the entire year from one for a week.
+     *
+     * @param  \GMP  $weeklyMap
+     * @return YearAvailabilityBitmap
+     */
     public static function fromWeeklyBitmap(GMP $weeklyMap)
     {
+        // First we need to convert the weekly bitmap to UTC.
+        // The challenge in this is when the shift implies
+        // availability that shifts from Sun to Mon and
+        // vice versa. Uncertain how we'll tackle it.
+        // TODO: implement conversion
 
+        // Once converted, we need to figure out on what day the 1st of
+        // January occurs, perform a right shift to get rid of every
+        // day before that, and finally create our binary string.
+        // The rest is done by left shifting and adding weeks.
+        $daysToShift = Carbon::create(null, 1, 1, 0, 0, 0)->day ?: 7;
+        $daysToShift--;
+        $yearlyMap = gmp_shiftr($weeklyMap, $daysToShift * BitwiseManipulator::INTERVALS_PER_DAY);
+
+        for ($i = 1; $i <= 52; $i++) {
+            $yearlyMap = gmp_shiftl($yearlyMap, Carbon::DAYS_PER_WEEK * BitwiseManipulator::INTERVALS_PER_DAY);
+            $yearlyMap = gmp_add($yearlyMap, $weeklyMap);
+        }
+
+        return new static($yearlyMap);
     }
 
+    /**
+     * @return \GMP|resource
+     */
     public function GMPInstance()
     {
         return $this->binary;
