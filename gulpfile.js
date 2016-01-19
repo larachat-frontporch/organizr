df049064589c40cb8183afe57be801be8be133a1
Sebastian Kraft <admin@helfull.de>
2016-01-19 22:29:55 +0100

    added font copy and versioning

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
     mix.sass('app.scss');
+
+    mix.browserify('app.js');
+
+    mix.copy('node_modules/materialize-css/font/*', 'public/build/font');
+
+    mix.version(['js/app.js', 'css/app.css']);
+
+    mix.browserSync({proxy: 'organizr.app'});
 });
