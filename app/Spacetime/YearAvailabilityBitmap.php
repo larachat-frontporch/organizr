b80560b24946c9f6b6356b6a1b55a492f5941d20
Leo Sjöberg <leo.sjoberg@gmail.com>
2016-01-19 19:21:04 +0200

    Added tests for the bitwise manipulator and a start to the yearly bitmap

---
+++ app/Spacetime/YearAvailabilityBitmap.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Spacetime;
+
+
+use GMP;
+
+class YearAvailabilityBitmap
+{
+    protected $binary;
+
+    /**
+     * Initialise the bitmap, ensure we always have a GMP object.
+     *
+     * @param $binary
+     */
+    public function __construct($binary)
+    {
+        if (! ($binary instanceof GMP)) {
+            $binary = gmp_init($binary, 2);
+        }
+
+        $this->binary = $binary;
+    }
+
+    public static function fromWeeklyBitmap(GMP $weeklyMap)
+    {
+
+    }
+}
---
+++ tests/BitwiseManipulatorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Spacetime\BitwiseManipulator;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class BitwiseManipulatorTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBinaryStringExpansionWithDefaults()
+    {
+        $this->assertEquals('0000111100001111', BitwiseManipulator::generateExpandedBinary([0,1,0,1]));
+    }
+
+    public function testBinaryStringExpansionWithStringArgument()
+    {
+        $this->assertEquals('0000111100001111', BitwiseManipulator::generateExpandedBinary('0101', 4, false));
+    }
+
+    public function testBinaryStringExpansionWithNonDefaultRepeatLength()
+    {
+        $this->assertEquals('00110011', BitwiseManipulator::generateExpandedBinary('0101', 2, false));
+    }
+}
