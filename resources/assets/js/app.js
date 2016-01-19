3ef4376dced52931f50374ce401471aa47a708a0
Sebastian Kraft <admin@helfull.de>
2016-01-19 22:30:46 +0100

    adds planner app javascript

---
+++ resources/assets/js/app.js
@@ -0,0 +1,55 @@
+var Vue = require('vue');
+
+Vue.config.debug = true;
+
+
+var app = new Vue({
+
+    el: '#app',
+
+    created: function() {
+        this.times = new Array(24 * 4 * 7);
+    },
+
+    data: {
+
+        times: [],
+
+        mouseDown: false,
+
+    },
+
+    methods: {
+
+        isDown: function(down) {
+            this.mouseDown = down;
+        },
+
+        onTimeClick: function(event, day, hour) {
+            if(event.type === 'mousedown') {
+                this.isDown(true);
+            }
+            if(this.mouseDown) {
+                for(var quarter = 0; quarter<4;quarter++) {
+                    var index = this.toIndex(day, hour, quarter);
+                    this.times[index] = !this.times[index];
+                }
+                event.target.classList.toggle('green');
+                event.target.classList.toggle('active');
+            }
+        },
+
+        isActive: function(day, hour) {
+            var index = this.toIndex(day, hour);
+            return this.times[index];
+        },
+
+        toIndex: function(day, hour, quarter) {
+            quarter = (typeof quarter === 'undefined') ? 'default' : quarter;
+            hour = (typeof hour === 'undefined') ? 'default' : hour;
+
+            return day * 24 * 4 + hour * 4 + quarter;
+        }
+    }
+
+});
