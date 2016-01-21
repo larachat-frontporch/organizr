3e2b975b02c91fc6ca0e4e031e1729aa2ef44f53
Helfull <admin@helfull.de>
2016-01-21 10:42:37 +0100

    added vendor.js compiler

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,13 @@ elixir(function(mix) {
     mix.browserify('app.js');
 
     mix.copy('node_modules/materialize-css/font/*', 'public/build/font');
+    mix.copy('node_modules/jquery/dist/jquery.js', 'resources/assets/js');
+    mix.copy('node_modules/materialize-css/bin/materialize.js', 'resources/assets/js');
+
+    mix.scripts([
+        'jquery.js',
+        'materialize.js'
+    ], 'public/build/js/vendor.js');
 
     mix.version(['js/app.js', 'css/app.css']);
 
